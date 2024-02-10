ddacd79a97cb738dc3e9a2046e9a7090b1f5eceb
Kesavan vel <you@example.com>
2024-02-10 18:51:35 +0530

    second commit

---
+++ StockPricePredictionApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('compare/',views.compare,name='compare'),
+    path('download/<id>',views.download,name='download'),
+    path('predict/',views.predict,name='predict'),
+    path('details/<id>',views.details,name='details'),
+]
